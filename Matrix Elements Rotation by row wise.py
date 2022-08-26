7bb93e944e47c2323ac74aa3b6c1042975a8d17b
DpsJoshi <112167326+DpsJoshi@users.noreply.github.com>
2022-08-26 18:58:51 +0530

    Update Matrix Elements Rotation by row wise.py

--- Matrix Elements Rotation by row wise.py
+++ Matrix Elements Rotation by row wise.py
@@ -2,42 +2,42 @@ def matrix_clockwise(arr):
     if len(arr)==0:
         return
     top=0
-    left=0
+    left=0                                  #Intialize the values of corners
     right=len(arr)-1
     bottom=len(arr) - 1
-    new_prev = 0
+    new_prev = 0                            #Intialize for previous element condition
 
     while top<=bottom:
 
-        if len(arr)%2==0 and top==0:
+        if len(arr)%2==0 and top==0:           #For even rows matrix last element should be 1st of last rowth.
             prev=arr[bottom][left]
-        elif len(arr)%2==1 and top==0:
+        elif len(arr)%2==1 and top==0:         #For odd rows matrix last element should be last of last rowth.
             prev=arr[bottom][right]
         else:
             prev=new_prev
             new_prev-=prev
 
-        for i in range(left,right+1):
+        for i in range(left,right+1):           #Swaping the elements of even lines
             cur=arr[top][i]
             arr[top][i]=prev
             prev=cur
 
         top+=1
-        if top>bottom:
+        if top>bottom:                           #Break the loop if there is no more rows
             break
 
         for i in range(len(arr)-1,-1,-1):
             cur=arr[top][i]
-            arr[top][i]=prev
+            arr[top][i]=prev                     #Swaping the elemts of odd rows
             prev=cur
             if i==0:
                 new_prev+=prev
         top+=1
-        if top>bottom:
+        if top>bottom:                            #Break the loop if there is no more rows
             break
 
     for row in arr:
-        print(row)
+        print(row)                                 #For printing the new array
 
 arr1=([[1,2,3],
        [4,5,5],
